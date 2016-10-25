6be944d43ac6af623280dd093d38d958d4a813b5
Alex Thompson <godfoder@acis.ufl.edu>
2016-10-25 14:43:55 -0400

    move run to main function.

--- rda_collections_api/__init__.py
+++ rda_collections_api/__init__.py
@@ -4,4 +4,8 @@ from connexion.resolver import RestyResolver
 app = connexion.App(__name__)
 app.add_api('swagger.yaml', arguments={"app_name": __name__})
 
-app.run(port=8080)
+def main():
+	app.run(port=8080)
+
+if __name__ == '__main__':
+	main()
