371cc5a050762f1982786f103974df2c93e40db1
Alex Thompson <godfoder@acis.ufl.edu>
2016-11-10 09:26:12 -0500

    listen on all ips.

--- rda_collections_api/__main__.py
+++ rda_collections_api/__main__.py
@@ -3,7 +3,7 @@ from rda_collections_api import app, redis_store
 def main():
 
     redis_store.init_app(app.app)
-    app.run(port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 if __name__ == '__main__':
     main()
