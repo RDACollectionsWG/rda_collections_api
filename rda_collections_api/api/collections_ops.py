2b6acd47b4d8e2f175a1184f9e220fe19183e856
Alex Thompson <godfoder@acis.ufl.edu>
2017-01-31 09:57:20 -0500

    add untested implementations of instersection and union.

--- rda_collections_api/api/collections_ops.py
+++ rda_collections_api/api/collections_ops.py
@@ -1,12 +1,38 @@
 from flask import request
 from rda_collections_api import redis_store
 from rda_collections_api.api import decode_redis, encode_redis
+from rda_collections_api.api.members import get as getMember
 
 def matching_members():
     return {}
 
-def intersection():
-    return {}
+def intersection(id, otherId):
+    contents = []
+    for m in redis_store.sinter(
+        "collections/{}".format(id),
+        "collections/{}".format(otherId)
+    ):
+        contents.append(getMember(id, m))
 
-def union():
+    return {
+        "contents": contents,
+        "next_cursor": "string",
+        "prev_cursor": "string"
+    }
+
+def union(id, otherId):
+    contents = []
+    for m in redis_store.sunion(
+        "collections/{}".format(id),
+        "collections/{}".format(otherId)
+    ):
+        contents.append(getMember(id, m))
+
+    return {
+        "contents": contents,
+        "next_cursor": "string",
+        "prev_cursor": "string"
+    }
+
+def flatten():
     return {}
