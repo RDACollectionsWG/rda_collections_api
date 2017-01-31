d08010f29776db121169742efb25bf2e25ed7f92
Alex Thompson <godfoder@acis.ufl.edu>
2017-01-31 09:56:13 -0500

    formatting members.

--- rda_collections_api/api/members.py
+++ rda_collections_api/api/members.py
@@ -15,24 +15,24 @@ def members_list(id):
 def post(id):
     mbody = request.get_json()
     redis_store.sadd("collections/{}/members".format(id), mbody["id"])
-    redis_store.hmset("collections/{}/members/{}".format(id,mbody["id"]), encode_redis(mbody))
+    redis_store.hmset("collections/{}/members/{}".format(id, mbody["id"]), encode_redis(mbody))
     return mbody
 
 def get(id, mid):
-    return decode_redis(redis_store.hgetall("collections/{}/members/{}".format(id,mid)))
+    return decode_redis(redis_store.hgetall("collections/{}/members/{}".format(id, mid)))
 
 def put(id, mid):
     mbody = request.get_json()
-    redis_store.hmset("collections/{}/members/{}".format(id,mid), encode_redis(mbody))
+    redis_store.hmset("collections/{}/members/{}".format(id, mid), encode_redis(mbody))
     return NoContent, 202
 
 def delete(id, mid):
     redis_store.srem("collections/{}/members".format(id), mid)
-    redis_store.delete("collections/{}/members/{}".format(id,mid))
+    redis_store.delete("collections/{}/members/{}".format(id, mid))
     return NoContent, 200
 
 def get_property(id, mid, property):
-    return decode_redis(redis_store.hget("collections/{}/members/{}".format(id,mid), property))
+    return decode_redis(redis_store.hget("collections/{}/members/{}".format(id, mid), property))
 
 def post_property(id, mid, property):
     raise NotImplemented
